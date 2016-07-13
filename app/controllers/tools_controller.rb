4e2b61a73fb65737dd12384cba428dfa3d78e998
matthewrpacker <matthewrpacker@gmail.com>
2016-07-13 11:58:24 -0600

    Begin to CRUD

---
+++ app/controllers/tools_controller.rb
@@ -0,0 +1,46 @@
+class ToolsController < ApplicationController
+
+  def index
+    @tools = Tool.all
+  end
+
+  def create
+    tool = Tool.new(tool_params)
+    if tool.save
+      redirect_to tools_path
+    else
+      render :new
+    end
+  end
+
+  def new
+    @tool = Tool.new
+  end
+
+  def edit
+    @tool = Tool.find(params[:id])
+  end
+
+  def show
+    @tool = Tool.find(params[:id])
+  end
+
+  def update
+    @tool = Tool.find(params[:id])
+    if @tool.update_attributes(tool_params)
+      redirect_to tools_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+  def tool_params
+    params.require(:tool).permit(:name, :quantity, :price)
+  end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+Tool.create(name: "Rotary cutter", price: 100.00, quantity: 10)
+Tool.create(name: "Hammer", price: 15.99, quantity: 10)
+Tool.create(name: "Rake", price: 20.00, quantity: 10)
+Tool.create(name: "Cheese slicer", price: 5.99, quantity: 10)
+Tool.create(name: "Saw", price: 19.99, quantity: 10)
+Tool.create(name: "Water timer", price: 23.99, quantity: 10)#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
